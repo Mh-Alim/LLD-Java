a1afb0383744d2be53b13ff30df13b3338402df0
Alim Khan <alim.khan@moneyview.in>
2025-11-27 01:00:46 +0530

    Strategy Pattern Practice

--- src/StrategyPattern/example1.java
+++ src/StrategyPattern/example1.java
@@ -1,4 +1,99 @@
 package StrategyPattern;
 
+
+/*
+class Vehicle {
+    public void drive() {
+        System.out.println("Driving Capability: Normal");
+    }
+}
+
+
+class SportsVehicle extends Vehicle{
+    @Override
+    public void drive() {
+        System.out.println("Driving Capability: Sports");
+    }
+}
+
+class OffRoadVehicle extends Vehicle{
+    @Override
+    public void drive() {
+        System.out.println("Driving Capability: Sports"); // Code Duplication.
+    }
+}
+
+class PassangerVehicle extends Vehicle{
+}
+
+
+ */
+
+interface DrivingStrategy {
+    void drive();
+}
+
+class SportsDrivingStrategy implements DrivingStrategy{
+    @Override
+    public void drive() {
+        System.out.println("Driving Capability: Sports");
+    }
+}
+
+class NormalDrivingStrategy implements DrivingStrategy{
+    @Override
+    public void drive() {
+        System.out.println("Driving Capability: Normal");
+    }
+}
+
+
+class Vehicle {
+    DrivingStrategy drivingStrategy;
+    Vehicle(DrivingStrategy drivingStrategy){
+        this.drivingStrategy = drivingStrategy;
+    }
+
+    public void drive(){
+        drivingStrategy.drive();
+    }
+}
+
+class SportsVehicle extends Vehicle{
+    SportsVehicle(DrivingStrategy drivingStrategy){
+        super(drivingStrategy);
+    }
+}
+
+class NormalVehicle extends Vehicle{
+    NormalVehicle(DrivingStrategy drivingStrategy){
+        super(drivingStrategy);
+    }
+}
+
+class OffRoadVehicle extends Vehicle{
+    OffRoadVehicle(DrivingStrategy drivingStrategy){
+        super(drivingStrategy);
+    }
+}
+
 public class example1 {
+    public static void main(String[] args) {
+        /*
+
+        SportsVehicle sportsVehicle = new SportsVehicle();
+        OffRoadVehicle offRoadVehicle = new OffRoadVehicle();
+        PassangerVehicle passangerVehicle = new PassangerVehicle();
+        sportsVehicle.drive();
+        offRoadVehicle.drive();
+        passangerVehicle.drive();
+
+        */
+
+        SportsVehicle sportsVehicle = new SportsVehicle(new SportsDrivingStrategy());
+        sportsVehicle.drive();
+
+        NormalVehicle normalVehicle = new NormalVehicle(new NormalDrivingStrategy());
+        normalVehicle.drive();
+    }
 }
--- src/StrategyPattern/paymentExample.java
+++ src/StrategyPattern/paymentExample.java
@@ -1,4 +1,63 @@
 package StrategyPattern;
 
+
+
+interface PaymentStrategies {
+    void pay();
+}
+
+class NetBanking implements PaymentStrategies {
+    @Override
+    public void pay() {
+        System.out.println("Paid via Netbanking");
+    }
+}
+
+class CreditCard implements PaymentStrategies {
+    @Override
+    public void pay() {
+        System.out.println("Payment via CreditCard");
+    }
+}
+
+class Upi implements PaymentStrategies {
+    @Override
+    public void pay() {
+        System.out.println("Payment via Upi");
+    }
+}
+
+class DebitCard implements PaymentStrategies {
+    @Override
+    public void pay() {
+        System.out.println("Payment via DebitCard");
+    }
+}
+
+
+class Checkout {
+    PaymentStrategies strategy;
+    public Checkout(PaymentStrategies strategy) {
+        this.strategy = strategy;
+    }
+    public void makePayment(){
+        // we can make payment using different method like CC, NET_BANKING, UPI, DEBIT_CARD -> these are different strategies
+        strategy.pay();
+    }
+}
 public class paymentExample {
+
+    public static void main(String[] args) {
+        Checkout checkout1 = new Checkout(new CreditCard());
+        checkout1.makePayment();
+
+        Checkout checkout2 = new Checkout(new NetBanking());
+        checkout2.makePayment();
+
+        Checkout checkout3 = new Checkout(new Upi());
+        checkout3.makePayment();
+
+        Checkout checkout4 = new Checkout(new DebitCard());
+        checkout4.makePayment();
+    }
 }
